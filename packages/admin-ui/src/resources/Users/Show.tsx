39380064f20e098a70ccbf130b77de699076239d
Chad Faurie <chad@bluetrino.com>
2023-11-07 11:31:49 +0200

    fix(user): added access lists

--- packages/admin-ui/src/resources/Users/Show.tsx
+++ packages/admin-ui/src/resources/Users/Show.tsx
@@ -1,11 +1,43 @@
-import { Show, SimpleShowLayout, TextField } from "react-admin";
+import {
+  ChipField,
+  Datagrid,
+  DeleteWithConfirmButton,
+  ReferenceManyField,
+  Show,
+  Tab,
+  TabbedShowLayout,
+  TextField,
+} from "react-admin";
+
+import { ProjectReferenceField, PropertyReferenceField } from "../../references";
 
 export const UserProfileShow = () => (
   <Show>
-    <SimpleShowLayout>
-      <TextField source="first_name" />
-      <TextField source="last_name" />
-      <TextField source="email" />
-    </SimpleShowLayout>
+    <TabbedShowLayout>
+      <Tab label="Summary">
+        <TextField source="first_name" />
+        <TextField source="last_name" />
+        <TextField source="email" />
+        <ChipField source="role" />
+      </Tab>
+      <Tab label="Project Access">
+        <ReferenceManyField label={false} reference="user_project_role_link" source="id" target="user_id">
+          <Datagrid bulkActionButtons={false}>
+            <ProjectReferenceField source="project_id" />
+            <ChipField source="role" />
+            <DeleteWithConfirmButton />
+          </Datagrid>
+        </ReferenceManyField>
+      </Tab>
+      <Tab label="Property Access">
+        <ReferenceManyField label={false} reference="user_property_role_link" source="id" target="user_id">
+          <Datagrid bulkActionButtons={false}>
+            <PropertyReferenceField source="property_id" />
+            <ChipField source="role" />
+            <DeleteWithConfirmButton />
+          </Datagrid>
+        </ReferenceManyField>
+      </Tab>
+    </TabbedShowLayout>
   </Show>
 );
