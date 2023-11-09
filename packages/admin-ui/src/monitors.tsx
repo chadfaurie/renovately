ab5a24aeb7adec4afc33e13d1b615468982d6f3d
Chad Faurie <chad@bluetrino.com>
2023-11-09 12:26:57 +0200

    fix(sentry): only enabled in envs that are not dev

--- packages/admin-ui/src/monitors.tsx
+++ packages/admin-ui/src/monitors.tsx
@@ -1,19 +1,19 @@
 import * as Sentry from "@sentry/react";
 
-Sentry.init({
-  dsn: "https://1e0399b666b3b720a46ef3102971bab0@o1250054.ingest.sentry.io/4506194636636160",
-  integrations: [
-    new Sentry.BrowserTracing({
-      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-    }),
-    new Sentry.Replay(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 0.1, // Capture 100% of the transactions
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
-
-console.log("Sentry Init");
+if (process.env.NODE_ENV !== "development") {
+  Sentry.init({
+    dsn: "https://1e0399b666b3b720a46ef3102971bab0@o1250054.ingest.sentry.io/4506194636636160",
+    integrations: [
+      new Sentry.BrowserTracing({
+        // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+        tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+      }),
+      new Sentry.Replay(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 0.1, // Capture 100% of the transactions
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+}
